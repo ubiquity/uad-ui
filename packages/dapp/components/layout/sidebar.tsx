1f1657b1020bb9e0bd8efa6b813e7c5534c113e1
rndquu <rndquu@gmail.com>
2024-05-06 13:01:15 +0300

    refactor: remove unused files

--- packages/dapp/components/layout/sidebar.tsx
+++ packages/dapp/components/layout/sidebar.tsx
@@ -31,9 +31,7 @@ const Sidebar = () => {
             <li>
               <hr />
             </li>
-            <Item text="Staking" href="/staking" icon="⛏"></Item>
-            <Item text="Credits" href="/credits" icon="💸"></Item>
-            <Item text="Markets" href="/markets" icon="🔁"></Item>
+            <Item text="Pool" href="/pool" icon="💸"></Item>
           </ul>
 
           <ul>
